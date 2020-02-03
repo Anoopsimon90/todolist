0d0cc52a473086a2375b5c92611dedf9e1e79c62
Anoop <simon.anoopsimon.anoop@gmail.com>
2020-02-04 00:39:17 +1100

    Fixed alignment issue

--- ToDoList/ClientApp/src/components/Home.js
+++ ToDoList/ClientApp/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component,Form } from 'react';
-import { Button,Alert, Row } from 'react-bootstrap'; 
+import { Button,Alert, Row,Col } from 'react-bootstrap'; 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -19,7 +19,10 @@ export class Home extends Component {
       },name:'',
       description:'',
       category:'*Select Category*',
-      priority:''
+      priority:'',
+      style: {
+        border: "20px solid black"
+    },
       
     };
 
@@ -54,39 +57,61 @@ export class Home extends Component {
         <p>Welcome {this.state.texts.userName}</p>
         </Alert>
         </div>
-
+<div class="col-md-6 col-md-offset-3" style={this.style}> 
 <form onSubmit={this.AddItem}>
+
   <Row>
- <label>Item Name    
+  <Col xs="3">
+ <label>Item Name    </label>
+ </Col>
+
+ <Col xs="3">
+
  <input type="text" name='name' value={this.state.name} onChange={this.handleChanges} />
-  </label>
+ </Col>
+
   </Row>
   <Row>
-  <label>Priority  
+  <Col xs="3">  <label>Priority  </label>  </Col>
+
+  <Col xs="3">
   <select value={this.state.category} onChange={this.handleChanges} name='priority'>
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
-      </select>  </label>
+      </select>  
+      </Col >
+
   </Row>
   <Row>
-  <label>Description  
-  <input type="text" name='description' value={this.state.description} onChange={this.handleChanges} />
-  </label>
+  <Col xs="3">
+
+  <label>Description  </label>  </Col>
+  <Col xs="3">
+ <input type="text" name='description' value={this.state.description} onChange={this.handleChanges} />
+ </Col>
   </Row>
   <Row>
-  <label>Category  
+  <Col xs="3">
+  <label>Category  </label>
+  </Col>
+  <Col xs="3">
   <select value={this.state.category} onChange={this.handleChanges}>
         <option value="Sports">Sports</option>
         <option value="Work">Work</option>
         <option value="Personal">Personal</option>
       </select>
-  </label>
+      </Col>
   </Row>
   <Row>
+  <Col xs="3"/>
+
+  <Col xs="3">
   <input type="submit" value="Submit" />
+  </Col>
   </Row>
 </form>
+</div>
         </div>
     );
   }
